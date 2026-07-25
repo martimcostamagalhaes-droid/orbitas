e3295e07b185615d6fd61cc8faec6b484ba4b869
Matt <martimcostamagalhaes@gmail.com>
2026-07-25 19:17:26 +0100

    grafico 2D

--- orbita2D.py
+++ orbita2D.py
@@ -1,4 +1,5 @@
 import math
+import matplotlib.pyplot as plt
 G = 6.674 * 10 ** (-11)
 Mt = 5.972 * 10 ** 24
 Rt = 6.371 * 10 ** 6
@@ -13,13 +14,26 @@ ysat = 0
 vx = 0
 vy = 7672
 dt = 1           
-tempo_total = 100
+tempo_total = 6000
 tempo_atual = 0
+historico_x = []
+historico_y = []
 while tempo_atual < tempo_total:
     ax, ay = calcular_aceleracao(xsat, ysat)
     vx = vx + ax * dt
     vy = vy + ay * dt
     xsat = xsat + vx * dt
     ysat = ysat + vy * dt
+    historico_x.append(xsat)
+    historico_y.append(ysat)
     tempo_atual = tempo_atual + dt
-    print("posicao final X:", xsat, "Y:", ysat)
+plt.figure(figsize=(8, 8))
+plt.plot(historico_x, historico_y, label="orbita do satelite", color="blue")
+plt.plot(0, 0, 'go', markersize=20, label="terra")
+plt.title("simulacao de orbita 2D")
+plt.xlabel("posicao X (m)")
+plt.ylabel("posicao Y (m)")
+plt.axis("equal")
+plt.legend()
+plt.grid()
+plt.show()
